0fcea25f82526a6233120a7a5e14f3f6d2ec7f78
Jarod Florence <jarod.florence@outlook.com>
2022-04-27 12:22:55 -0500

    modules created

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require("webpack");
 
@@ -10,6 +11,10 @@ module.exports = {
   plugins: [new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
-  }),],
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder / Use disable value to temp. stop report to browser
+  })
+],
   mode: 'development'
 };
